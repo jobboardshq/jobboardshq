c41bf3941025104530c47a0f55cfdb81a60104a8
shabda <shabda@f7cac328-5256-41e8-ba92-3b25492a8a37>
2008-11-06 07:33:37 +0000

    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@34 f7cac328-5256-41e8-ba92-3b25492a8a37

--- zobpress/middleware.py
+++ zobpress/middleware.py
@@ -10,6 +10,12 @@ class GetSubdomainMiddleware:
         bits = urlparse.urlsplit(request.META['HTTP_HOST'])[0].split('.')
         if not( len(bits) == 3):
             pass#Todo Raise an exception etc
+            bits = urlparse.urlsplit(request.META['HTTP_HOST'])[3].split('.')
+            if not( len(bits) == 3):
+                bits = request.META['HTTP_HOST'].split('.')
+                if not( len(bits) == 3):
+                    pass
+                    #raise Exception("There was a problem getting the subdomain")
         request.subdomain = bits[0]
         try:
             board = Board.objects.get(subdomain = request.subdomain)
