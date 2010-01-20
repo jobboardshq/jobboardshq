5cbecd8a2f1d79323040578f29d8c82199dce9d9
shabda <shabda@f7cac328-5256-41e8-ba92-3b25492a8a37>
2010-01-20 11:42:35 +0000

    Templates around widgets
    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@57 f7cac328-5256-41e8-ba92-3b25492a8a37

--- widgets/urls.py
+++ widgets/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('widgets.views',
-    url('$', 'index', name='widgets_index'),
-    url('^jobs.js$', 'jobs_js', name='widgets_jobs_js'),
-    url('^jobs/$', 'jobs_iframe', name='widgets_jobs'),
-    url('^test/$', 'test_widgets', name='widgets_test'),
+    url(r'^$', 'index', name='widgets_index'),
+    url(r'^jobs.js$', 'jobs_js', name='widgets_jobs_js'),
+    url(r'^jobs/$', 'jobs_iframe', name='widgets_jobs'),
+    url(r'^test/$', 'test_widgets', name='widgets_test'),
 )
 
     
