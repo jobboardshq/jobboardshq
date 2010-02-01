79c9d510fc2bbd4630e5d1412d383bc85775a264
shabda <shabda@f7cac328-5256-41e8-ba92-3b25492a8a37>
2010-02-01 14:54:18 +0000

    Refs #26, #12
    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@102 f7cac328-5256-41e8-ba92-3b25492a8a37

--- zobpress/search_indexes.py
+++ zobpress/search_indexes.py
@@ -3,8 +3,10 @@ from haystack import indexes, site
 
 from zobpress.models import Job
 
-class JobIndex(indexes.SearchIndex):
+class JobIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
-    as_clob = indexes.CharField(model_attr = "as_clob")
+    as_clob = indexes.CharField()
+    name = indexes.CharField()
+    description = indexes.CharField()
     
 site.register(Job, JobIndex)
