3a32af34fb84d51c5787fa9b5b84a7435d84bc1e
shabda <shabda@f7cac328-5256-41e8-ba92-3b25492a8a37>
2010-02-01 13:57:12 +0000

    Refs #26. Templates for the sales pages.
    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@99 f7cac328-5256-41e8-ba92-3b25492a8a37

--- sitewide/urls.py
+++ sitewide/urls.py
@@ -3,6 +3,11 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('sitewide.views',
     url(r'^$', 'index', name='sitewide_index'),
+    url(r'^features/$', direct_to_template, {"template":"sitewide/features.html"}, name='sitewide_features'),
+    url(r'^demo/$', direct_to_template, {"template":"sitewide/demo.html"}, name='sitewide_demo'),
+    url(r'^pricing/$', direct_to_template, {"template":"sitewide/pricing.html"}, name='sitewide_pricing'),
+    url(r'^support/$', direct_to_template, {"template":"sitewide/support.html"}, name='sitewide_support'),
+    url(r'^contact/$', direct_to_template, {"template":"sitewide/contact.html"}, name='sitewide_contact'),
     url(r'^register/$', 'register_board', name='sitewide_register_board'),
 )
 
