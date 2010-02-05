b62c61f8b2c5313be3a819b9b3b48cb37da4a3a5
shabda <shabda@f7cac328-5256-41e8-ba92-3b25492a8a37>
2010-02-05 11:07:06 +0000

    added document.domain
    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@140 f7cac328-5256-41e8-ba92-3b25492a8a37

--- zobpress/context_processor.py
+++ zobpress/context_processor.py
@@ -1,4 +1,5 @@
 from zobpress.models import Board, Category
+from django.conf import settings
 
 def populate_board(request):
     "Populate the board in the template"
@@ -7,4 +8,6 @@ def populate_board(request):
         categories = Category.objects.filter(board = board)
     else:
         categories = []
-    return {'board':request.board, 'categories':categories, 'current_url': request.META['PATH_INFO']}
+    return {'board':request.board, 'categories':categories, 'current_url': request.META['PATH_INFO'], "base_domain": settings.BASE_DOMAIN}
+
+    
