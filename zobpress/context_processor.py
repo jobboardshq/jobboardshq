b6e14726df6063c1e5e9966756f4715a2db7a7e1
ashok <ashok@f7cac328-5256-41e8-ba92-3b25492a8a37>
2010-02-16 13:35:09 +0000

    populate is_board_admin context processor based on valid board
    git-svn-id: https://agiliq.unfuddle.com/svn/agiliq_zobpress@251 f7cac328-5256-41e8-ba92-3b25492a8a37

--- zobpress/context_processor.py
+++ zobpress/context_processor.py
@@ -7,12 +7,14 @@ from zobpress.models import Board, Category
 def populate_board(request):
     "Populate the board in the template"
     board = request.board
+    is_board_admin = False
     if board:
         categories = Category.objects.filter(board = board)
         board_settings = board.settings
+        is_board_admin = can_access_board_backend(request)
     else:
         categories = []
         board_settings = None
-    return {'board':request.board, 'categories':categories, "board_settings": board_settings, 'current_url': request.META['PATH_INFO'], "base_domain": settings.BASE_DOMAIN, 'debug': settings.DEBUG, 'is_board_admin': can_access_board_backend(request)}
+    return {'board':request.board, 'categories':categories, "board_settings": board_settings, 'current_url': request.META['PATH_INFO'], "base_domain": settings.BASE_DOMAIN, 'debug': settings.DEBUG, 'is_board_admin': is_board_admin}
 
     
